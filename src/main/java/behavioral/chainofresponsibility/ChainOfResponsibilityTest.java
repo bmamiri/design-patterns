9b1470ff2d8b216a2cd11f374cbbfaa56b601c2f
Mohammad Amiri <b.m.amiri@live.com>
2020-04-01 22:46:49 +0430

    Chain of Responsibility - Code Improved

--- src/main/java/behavioral/chainofresponsibility/ChainOfResponsibilityTest.java
+++ src/main/java/behavioral/chainofresponsibility/ChainOfResponsibilityTest.java
@@ -2,7 +2,7 @@ package behavioral.chainofresponsibility;
 
 import behavioral.chainofresponsibility.service.MessageService;
 
-public class Test {
+public class ChainOfResponsibilityTest {
     public static void main(String[] args) {
         Message message = Message.getBuilder().setValue("Hi, New Message")
                 .addDestination(Message.Destination.SMS, "1234567890")
